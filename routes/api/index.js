f9cb0396cb75b9e5aa799fc29a44c4c9c0e990dd
Sihayah <sihaya_a@hotmail.com>
2021-12-29 07:20:51 -0500

    notes posting

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,13 +1,32 @@
+const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
-const db = require('../../db/db.json');
+const notes = require('../../db/notes.json');
+
+function writeNote(body, notesArr) {
+    const note = body;
+    notesArr.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, '../../db/notes.json'),
+      JSON.stringify({ notes: notesArr }, null, 2)
+    );
+    return note;
+  }
 
 router.get('/notes', (req, res) => {
     console.log('getting...')
-    if(db) {
-        res.json(db);
+    if(notes) {
+        res.json(notes);
     } else {
         res.send(404)
     }
-})
+});
+
+router.post('/notes', (req, res) => {
+    console.log('posting...');
+    // req.body.id = notes.length.toString();
+    const note = writeNote(req.body, notes);
+    res.json(note);
+});
 
 module.exports = router;
