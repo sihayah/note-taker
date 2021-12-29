11772ba74fd631bdfdc6d714bb4a6caaf11ff3c0
Sihayah <sihaya_a@hotmail.com>
2021-12-29 09:11:43 -0500

    added uuid

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const notes = require('../../db/notes.json');
-const { writeNote, deleteNote } = require('../../lib/notes')
+const { writeNote, deleteNote } = require('../../lib/notes');
+
 
 router.get('/notes', (req, res) => {
     console.log('getting...')
@@ -12,7 +14,9 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = (notes.length+1).toString();
+    req.body.id = uuidv4();
+    console.log(req.body.id)
+    // req.body.id = (notes.length+1).toString();
     console.log('posting...');
     const note = writeNote(req.body, notes);
     res.json(note);
