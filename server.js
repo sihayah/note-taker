62a2854e42e40c906376e26542949c2f11c9e00a
Sihayah <sihaya_a@hotmail.com>
2021-12-29 04:14:43 -0500

    server updated

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// // parse incoming string or array data
+// app.use(express.urlencoded({ extended: true }));
+// // parse incoming JSON data
+// app.use(express.json());
+app.use(express.static('public'));
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`server now listening on port ${PORT}`)
+});
