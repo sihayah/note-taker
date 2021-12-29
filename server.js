75749116637d0f686f910a4e60083a348a797648
Sihayah <sihaya_a@hotmail.com>
2021-12-29 09:28:21 -0500

    bug fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const apiRoutes = require('./routes/api');
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
-app.use(express.json());\
+app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
