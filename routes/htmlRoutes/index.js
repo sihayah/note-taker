f6c3bf6287f9b78b0e1c064a5247955b00cb3487
Sihayah <sihaya_a@hotmail.com>
2021-12-29 09:18:54 -0500

    comments

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
+// route to notes page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+// route to landing page
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
